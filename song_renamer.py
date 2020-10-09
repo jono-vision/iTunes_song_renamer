dd2f6070d9af8c2afee2a922a591636fe1a01489
jono-vision <60055216+jono-vision@users.noreply.github.com>
2020-10-09 12:35:33 -0600

    Updated to move featuring artist

--- song_renamer.py
+++ song_renamer.py
@@ -12,34 +12,30 @@ iTunes id3 tag renamer to remove featured artists in the artist tag and all colu
 
 from mutagen.mp4 import MP4
 import requests, os
-directory = '/Volumes/Media Drive/iTunes/iTunes Media/Music/Compilations/Special Occasion'
+directory = '/Volumes/Media Drive/iTunes/iTunes Media/Music/Compilations/JESUS IS KING'
 os.chdir(directory)
 
+splitter_list = ['feat.', 'ft.', 'featuring','Feat.', 'Ft.', 'Featuring', 'feat', 'Feat']
 artist_name = ''
 
 for folder,subfolder,filenames in os.walk(directory):
-    #file = '09 In Distress.m4a'
+    
     for file in filenames:
         audio = MP4(file)
-        song=audio['©ART'][0].split()
-        song+=audio['©nam'][0].split()
-        site = 'https://itunes.apple.com/search?term=' + '+'.join(song) + '&limit=1'
-        text = ' '.join(song)
+        artist = audio['©ART'][0]
+        splitter = list(set(artist.split()) & set(splitter_list)) # Find the variant of feature used
+        
+        if splitter == []: #If there is no featured artist in the artist tag skip to next file
+            continue
+        splitter = splitter[0].center(len(splitter)+2) #pads the string is spaces
+        split_artist = artist.split(splitter)
+        featured_artist = split_artist[1]
 
-        res = requests.get(site)
-        try:
-            res.raise_for_status()
-            if len(res.json()['results']) == 0:
-                print(f'No result found for {text}')
-            else:
-                info = res.json()['results'][0]
-                audio['©ART'] = info['artistName']
-                audio['©nam'] = info['trackName']
-                audio['©day'] = info['releaseDate']
-    
-        except Exception as exc:
-            print(f'There was an issue with song: {text}')
-    
+        if artist_name == '': # first file
+            artist_name = split_artist[0]
+        
+        audio['©nam'] =  audio['©nam'][0] + ' (ft.' + featured_artist + ')'
+        audio['©ART'] = artist_name
         audio.save(file)
 
 
